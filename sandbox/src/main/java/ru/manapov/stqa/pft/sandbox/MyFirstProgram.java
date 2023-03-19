b9307008ba24017e955e8c51ffbdd36450d291f2
FriendlyPhantasm <31846539+FriendlyPhantasm@users.noreply.github.com>
2023-03-19 13:45:18 +0500

    Task 2. Distance between two points

--- sandbox/src/main/java/ru/manapov/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/manapov/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,6 +3,16 @@ package ru.manapov.stqa.pft.sandbox;
 public class MyFirstProgram {
     public static void main(String[] args) {
 
-        System.out.println("Hello, world!");
+        Point p1 = new Point(2,4);
+        Point p2 = new Point(3,2);
+
+        System.out.println("Coordinates of the first point:");
+        System.out.println("x = " + p1.x);
+        System.out.println("y = " + p1.y);
+        System.out.println("Coordinates of the second point:");
+        System.out.println("x = " + p2.x);
+        System.out.println("y = " + p2.y);
+
+        System.out.println("Distance between points:" + p2.distance(p1,p2));
     }
 }
