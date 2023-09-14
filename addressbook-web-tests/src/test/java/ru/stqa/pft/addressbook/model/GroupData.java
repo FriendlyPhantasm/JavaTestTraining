520270891115e263c52f11978ad871ba48520bcc
FriendlyPhantasm <31846539+FriendlyPhantasm@users.noreply.github.com>
2023-09-14 22:13:07 +0500

    Lesson 4.9

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -4,26 +4,26 @@ package ru.stqa.pft.addressbook.model;
 import java.util.Objects;
 
 public class GroupData {
-    private final String id;
+    private int id;
     private final String name;
     private final String header;
     private final String footer;
 
     public GroupData(String name , String header, String footer) {
-        this.id = null;
+        this.id = 0;
         this.name = name;
         this.header = header;
         this.footer = footer;
     }
 
-    public GroupData(String id, String name , String header, String footer) {
+    public GroupData(int id, String name , String header, String footer) {
         this.id = id;
         this.name = name;
         this.header = header;
         this.footer = footer;
     }
 
-    public String getId() {
+    public int getId() {
         return id;
     }
 
@@ -39,9 +39,8 @@ public class GroupData {
         return header;
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, header, footer);
+    public void setId(int max) {
+        this.id = id;
     }
 
     @Override
@@ -53,53 +52,18 @@ public class GroupData {
                 ", footer='" + footer + '\'' +
                 '}';
     }
-}
 
-/*public record GroupData(String id, String name, String header, String footer) {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         GroupData groupData = (GroupData) o;
-        return Objects.equals(id, groupData.id) && Objects.equals(name, groupData.name) && Objects.equals(header, groupData.header) && Objects.equals(footer, groupData.footer);
+        return id == groupData.id && Objects.equals(name, groupData.name) && Objects.equals(header, groupData.header) && Objects.equals(footer, groupData.footer);
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(id, name, header, footer);
     }
-
-    @Override
-    public String toString() {
-        return "GroupData{" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                '}';
-    }
-
 }
-
-public record GroupData(String name, String header, String footer) {
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        GroupData groupData = (GroupData) o;
-        return Objects.equals(id, groupData.id) && Objects.equals(name, groupData.name) && Objects.equals(header, groupData.header) && Objects.equals(footer, groupData.footer);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, header, footer);
-    }
-
-    @Override
-    public String toString() {
-        return "GroupData{" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                '}';
-    }
-
-}*/
 
