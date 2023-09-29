57939129fd8ff73e6aa8308eaa6586efe3f5f582
FriendlyPhantasm <31846539+FriendlyPhantasm@users.noreply.github.com>
2023-09-29 22:28:10 +0500

    Task 14 config files

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -6,10 +6,15 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.Browser;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
 
 public class ApplicationManager {
 
+    private final Properties properties;
     WebDriver wd;
 
     private SessionHelper sessionHelper;
@@ -19,11 +24,15 @@ public class ApplicationManager {
     private String browser;
 
     public ApplicationManager(String browser) {
+
         this.browser = browser;
+        properties = new Properties();
     }
 
-    public void init() {
+    public void init() throws IOException {
 
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
         if (Browser.CHROME.browserName().equals(browser)) {
             wd = new ChromeDriver();
         } else if (Browser.EDGE.browserName().equals(browser)) {
@@ -33,12 +42,12 @@ public class ApplicationManager {
         }
 
         wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
-        wd.get("http://localhost/addressbook");
+        wd.get(properties.getProperty("web.baseUrl"));
         groupHelper = new GroupHelper(wd);
         contactHelper = new ContactHelper(wd);
         navigationHelper = new NavigationHelper(wd);
         sessionHelper = new SessionHelper(wd);
-        sessionHelper.login("admin", "secret");
+        sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
     }
 
     public void stop() {
